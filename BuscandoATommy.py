432ccf3dfa5bae6667445b155eef4549a76e1683
Carlos Cervantes <85979189+AndreCerva@users.noreply.github.com>
2021-12-11 18:28:23 -0600

    update

--- BuscandoATommy.py
+++ BuscandoATommy.py
@@ -1,28 +1,28 @@
-import cv2
-from PIL import Image
-from azure.iot.hub import IoTHubRegistryManager
-from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
-from msrest.authentication import ApiKeyCredentials
-credentials = ApiKeyCredentials(in_headers={"Prediction-key": "Your Prediction key"})
-predictor = CustomVisionPredictionClient("Your End-Point", credentials)
-CONNECTION_STRING = "Your Connection string IoT Hub"
-DEVICE_ID = "ID device IoT Hub"
-def iothub_messaging(data):
+import cv2#Libreria OpenCV para procesamiento de imagenes(Visión artificial)
+from PIL import Image#Python Imaging Library, libreria para la edición y manipulación de imagenes
+from azure.iot.hub import IoTHubRegistryManager #Libreria de Azure para IoT Hub
+from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient#Libreria de Azure para Custom Vision service
+from msrest.authentication import ApiKeyCredentials#Libreria de Azure para autenticar credenciales de servicio
+credentials = ApiKeyCredentials(in_headers={"Prediction-key": "Your Prediction key"})#La clave para hacer uso del servicio de custom vision desplegado
+predictor = CustomVisionPredictionClient("Your End-Point", credentials)#Endpoint donde se encuentra nuestro servicio de custom vision desplegado
+CONNECTION_STRING = "Your Connection string IoT Hub"#Cadena de conección para el servicio de Azure IoT Hub.
+DEVICE_ID = "ID device IoT Hub"#ID con el que se ha registrado el dispositivo con el que se va a comunicar
+def iothub_messaging(data):#Función para enviar mensaje a nuestro dispositivo, recibe como parametro la información a mandar
     try:
-        registry_manager = IoTHubRegistryManager(CONNECTION_STRING)#Create IoTHubRegistryManager
-        print ( 'Sending message... ' )
-        registry_manager.send_c2d_message(DEVICE_ID, data)
-        input("Message send, please press Enter...\n")
-        im = Image.open('capture.png')
-        im.show()
-        exit(0)
-    except Exception as ex:
-        print ( "Unexpected error {0}" % ex )
-        exit(1)
+        registry_manager = IoTHubRegistryManager(CONNECTION_STRING)#Creamos un IoTHubRegistryManager que nos permitira mandar el mensaje a nuestro dispositivo
+        print ( 'Sending message... ' )#Indicamos en consola que estamos mandando el mensaje
+        registry_manager.send_c2d_message(DEVICE_ID, data)#Con ayuda del metodo enviamos un mensaje dando como parametro el dispositivo y el mensaje
+        input("Message send, please press Enter...\n")#Avisamos que hemos enviado el mensaje a nuestro dispositivo correctamente
+        im = Image.open('capture.png')#Abrimos la imagen que capturamos y mandamos al servicio de custom vision
+        im.show()#Mostramos la imagen que abrimos
+        exit(0)#Terminamos el programa con exito
+    except Exception as ex:#En caso de no poder enviar el mensaje se abre la excepción
+        print ( "Unexpected error {0}" % ex )#Se muestra que no se pudo mandar el mensaje por el inesperado error
+        exit(1)#Se termina el programa con error
         return
-    except KeyboardInterrupt:
-        print ( "IoT Hub C2D Messaging service sample stopped" )
-if __name__ == '__main__':
+    except KeyboardInterrupt:#Excepción debida a interrupción por teclado ctrl c 
+        print ( "IoT Hub C2D Messaging service sample stopped" )#Mostramos en pantalla pq se detuvo la ejecución del script
+if __name__ == '__main__':#Cuando se inicie la ejecución del programa, entrara a este condicional
     camera = cv2.VideoCapture(0,cv2.CAP_DSHOW)#abrir la cámara y completar la inicialización de la cámara
     camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)#Cambiamos el ancho de la imagen 
     camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)#Cambiamos la altura de la imagen
@@ -33,12 +33,12 @@ if __name__ == '__main__':
             if cv2.waitKey(1) & 0xFF == ord('t'):#Si se preciona la tecla: t , entra al condicional
                 cv2.imwrite('capture.png', image)#Se guarda el fotograma cuando se presiono la tecla
                 camera.release()#Apaga la camara
-                cv2.destroyAllWindows()
+                cv2.destroyAllWindows()#Destruir todas las ventanas que se hayan mostrado anteriormente con cv2
                 break#Sale del ciclo while
         else: #En caso de no poder capturar un fotograma
             print('Error al intentar ingresar a la camara')#Mostrar posible error de pq no se capturo fotograma
     with open("capture.png", mode="rb") as captured_image: #Abre la imagen guardada en formato de lectura
-        results = predictor.detect_image("Your ID Proyect CV", "Proyect Name CV", captured_image)
+        results = predictor.detect_image("Your ID Project CV", "Project Name CV", captured_image)
     for prediction in results.predictions:
         if prediction.probability > 0.1:
             data='TOMMY HAS BEEN FOUND'
