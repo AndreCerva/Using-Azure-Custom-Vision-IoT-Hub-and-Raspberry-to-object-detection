48a30b26d748d9c14a5e28fc4b6bf9c6abc440f8
Carlos Cervantes <85979189+AndreCerva@users.noreply.github.com>
2021-12-17 00:07:42 -0600

    update code Device

--- README.md
+++ README.md
@@ -9,3 +9,15 @@ Se requiere instalar las siguientes librerias para su correcto funcionamiento:
 * pip install msrest
 
 Tener en cuenta la relación de aspecto de la imagen cuando se modifique su tamaño
+
+*Consideraciones de codigo de RaspDevice.py*
+
+Se requiere instalar las siguientes librerias:
+* sudo pip install azure-iot-hub
+    *En caso de no tener instalado la libreria GPIO*
+    * sudo apt-get update.
+    * sudo apt-get upgrade.
+    * sudo apt-get install python-dev.
+    * sudo apt-get install pyton-rpi. gpio.
+
+Para la elección de los pines, mediante la numeración locación fisica, ver GPIO_RASP3B.png o equivalente a tu modelo de Rasp
---
+++ RaspDevice.py
@@ -0,0 +1,39 @@
+import RPi.GPIO as GPIO#librerias que nos permite trabajar con los pines de la rasp
+import time #Libreria para tiempos de espera
+from azure.iot.device import IoTHubDeviceClient#Libreria para hacer uso del servicio de Azure IoT Hub
+CONNECTION_STRING = "HostName=IoT-C2D.azure-devices.net;DeviceId=ID-01;SharedAccessKey=M93WenML4CpoDNv7m9RkMJPwYXxV4WVVL4Bcar7mNrI="
+pinledV=23 #Pin positivo al que ira conectado el led verde
+pinledR=24 #Pin positivo al que ira conectado el led rojo
+GPIO.setmode(GPIO.BOARD) #Numeraremos los pines del rasp con la numeración board (locación fisica)
+GPIO.setup(pinledV,GPIO.OUT)#Indicamos que en el pin del led v estará mandando señal
+GPIO.setup(pinledR,GPIO.OUT)#Indicamos que en el pin del led r estará mandando señal
+#Funcion que imprime los mensajes recibidos en la consola
+def message_handler(message): #Recibe el mensaje con todos sus atributos y metodos
+  print("Message received\n")#Parametro message es una instancia de la clase Message, tiene varios atributos y metodos
+  if message.data.decode() == 'TOMMY HAS BEEN FOUND': #Si el mensaje que se envia es que se encontro a tommy
+      GPIO.output(pinledR,GPIO.LOW) #Apagar el led rojo
+      GPIO.output(pinledV,GPIO.HIGH) #Encender led verde
+  else: #No se ha encontrado a tommy aún
+      GPIO.output(pinledV,GPIO.LOW) #Apagar led verde
+      GPIO.output(pinledR,GPIO.HIGH) #encender led rojo
+#Funcion para inicializar el cliente y esperar a recibir el mensaje de la nube al dispositivo
+def main():
+    GPIO.output(pinledR,GPIO.HIGH)# iniciamos en alto el led rojo que indica que no se ha encontrado aún
+    GPIO.output(pinledV,GPIO.LOW) # Iniciamos en bajo el led verde 
+    print ("Buscando a Tommy...")#indicamos que se empieza la busqueda
+    client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)#instanciamos el cliente 
+    print ("Esperando por un mensaje")#Estamos esperando algún mensaje
+    try:
+        # Attach the handler to the client
+        client.on_message_received = message_handler#Leemos el mensaje que se ha enviado a nuestro device 
+        while True: 
+            time.sleep(1000) #mil segundos esperando un mensaje, por poner un numero, puede ser otro.
+    except KeyboardInterrupt:
+        print("IoT Hub C2D Messaging stopped") #se detiene la ejecución debido a una interrupción de teclado
+    finally:
+        GPIO.cleanup()
+        print("Shutting down IoT Hub Client")#Terminamos con éxito la ejecución del programa
+        client.shutdown()#Se deja de esperar el envio de mensajes
+if __name__ == '__main__':
+    main()
+    
